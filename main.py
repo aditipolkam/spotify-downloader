56948e0fa89d56c251ecbd639a0a20a9e5be8546
Aditi Polkam <aditipolkam@gmail.com>
2022-01-18 11:27:10 +0530

    [ADD] get track details by url entered

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ load_dotenv()
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
-
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+#lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+link = input("Enter url of the song:")
 
-results = sp.artist_top_tracks(lz_uri)
-
-for track in results['tracks'][:10]:
-    print('track    :' + track['name'])
-    print('audio    :' + track['preview_url'])
-    print('cover art:' + track['album']['images'][0]['url'])
-    print()
+results = sp.track(link)
+#print(results)
+print("Track name: ",results['name'])
+print("Artist: ", results['album']['artists'][0]['name'])
+print("Release date: ", results['album']['release_date'])
+#for track in results['tracks'][:10]:
+   # print('track    :' + track['name'])
 
 
 

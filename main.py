7714b9bb2ee5f016c45cd72e4e01e2be1f8fc9ab
Aditi Polkam <aditipolkam@gmail.com>
2022-01-18 16:35:25 +0530

    [MODIFY] more details of the track

--- main.py
+++ main.py
@@ -15,10 +15,22 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 link = input("Enter url of the song:")
 
 results = sp.track(link)
-#print(results)
+print(results)
 print("Track name: ",results['name'])
-print("Artist: ", results['album']['artists'][0]['name'])
 print("Release date: ", results['album']['release_date'])
+print("Album Artist: ", )
+for artist in results['album']['artists']:
+    print(artist['name'] , " : " , artist['external_urls']['spotify'])
+
+print("Album type: ",results['album']['album_type'])
+
+print("Images:")
+for img in results['album']['images']:
+    print(img['url'])
+
+print("Popularity: ", results['popularity'])
+
+print("Duration of track: ","{:.2f}".format(results['duration_ms']/1000/60))
 #for track in results['tracks'][:10]:
    # print('track    :' + track['name'])
 

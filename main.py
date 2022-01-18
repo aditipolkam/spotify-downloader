de930040b5e77253ae73ca285ad36d86691662a8
Aditi Polkam <aditipolkam@gmail.com>
2022-01-18 22:43:54 +0530

    [ADD] support for 4 types of links [FIX] used dict

--- main.py
+++ main.py
@@ -12,27 +12,54 @@ auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=clien
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 #lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
-link = input("Enter url of the song:")
+link = input("Enter url:")
 
-results = sp.track(link)
-print(results)
-print("Track name: ",results['name'])
-print("Release date: ", results['album']['release_date'])
-print("Album Artist: ", )
-for artist in results['album']['artists']:
-    print(artist['name'] , " : " , artist['external_urls']['spotify'])
+l = link.split('/')
+#print(l)
 
-print("Album type: ",results['album']['album_type'])
+if l[3] == "track":
+    results = sp.track(link)
+    track = {
+        'name':results['name'],
+        'release_date':results['album']['release_date'],
+        'artist':results['album']['artists'],
+        'album_type':results['album']['album_type'],
+        'images':results['album']['images'],
+        'popularity':results['popularity'],
+        'duration':"{:.2f}".format(results['duration_ms']/1000/60)
+    }
+    print(track)
 
-print("Images:")
-for img in results['album']['images']:
-    print(img['url'])
+elif l[3] == "artist":
+    results = sp.artist(link)
+    #print(results)
+    artist = {
+        'id':results['id'],
+        'name':results['name'],
+        'followers':results['followers']['total'],
+        'genres':results['genres']
+    }
+    print(artist)
 
-print("Popularity: ", results['popularity'])
+elif l[3] == "album":
+    results = sp.album(link)
+    album = {
+        'name':results['name'],
+        'genres':results['genres'],
+        "popularity":results['popularity'],
+        "release_date":results['release_date'],
+        "total_tracks":results['total_tracks'],
+        'tracks':results['tracks']
+    }
+    print(album)
 
-print("Duration of track: ","{:.2f}".format(results['duration_ms']/1000/60))
-#for track in results['tracks'][:10]:
-   # print('track    :' + track['name'])
-
-
-
+elif l[3] == "playlist":
+    results = sp.playlist(link)
+    playlist = {
+        'name': results['name'],
+        'description': results['description'],
+        'owner_name':results['owner']['display_name'],
+        'tracks':results['tracks']['items']
+    }
+    print(playlist)
+#print(results)

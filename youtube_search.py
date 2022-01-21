74dcd5d79cbc7619cceaed79442dcbca432560a0
Aditi Polkam <aditipolkam@gmail.com>
2022-01-21 16:16:40 +0530

    [FIX] empty search results from ytmusic

--- youtube_search.py
+++ youtube_search.py
@@ -1,9 +1,16 @@
+import sys
 from ytmusicapi import YTMusic
 
 def search_song(name):
     ytm = YTMusic()
     result = ytm.search(name, filter="songs")
-    videoid = result[0]["videoId"]
+    try:
+        videoid = result[0]["videoId"]
+    except KeyError or IndexError:
+        videoid=""
+        print("Song not found.")
+        sys.exit()
+
     video_url = "https://www.youtube.com/watch?v="+videoid
     return video_url
 

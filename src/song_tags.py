205af2c235a7e90ae37f13cc487b0223c71f1939
Aditi Polkam <aditipolkam@gmail.com>
2022-01-23 12:39:47 +0530

    [FIX] remove cover image jpg file

--- src/song_tags.py
+++ src/song_tags.py
@@ -1,7 +1,5 @@
-from email.mime import audio
-from ensurepip import version
-from tabnanny import verbose
 import eyed3
+import os
 
 def add_tags(file, title, album_name, release_date, artists, cover_img):
     audiofile = eyed3.load(str(file))
@@ -17,6 +15,9 @@ def add_tags(file, title, album_name, release_date, artists, cover_img):
         audiofile.tag.save(version=(1, None, None))
         audiofile.tag.save(version = (2,3,0))
         audiofile.tag.save()
+
+        #remove cover img i.e. not required
+        os.remove(cover_img)
         print("Tags embedded.")
     else:
         print("File not loaded")

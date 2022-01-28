cc20bacf21e3d3ce721d7c5c72626c9f31264981
Aditi Polkam <aditipolkam@gmail.com>
2022-01-28 18:00:42 +0530

    [FIX] overwrite if block

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import pathlib
 from dotenv import load_dotenv
 
@@ -11,62 +12,61 @@ from song_tags import add_tags
 
 load_dotenv()
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 if "downloads" not in os.listdir():
     os.mkdir("downloads")
 
-save_path = './downloads/'
+save_path = "./downloads/"
 
 url = input("Enter url: ")
-res = Spot(client_id=client_id, client_secret = client_secret).get_info(url)
-#print(res)
+res = Spot(client_id=client_id, client_secret=client_secret).get_info(url)
+# print(res)
 
 
-
-for track in res['tracks']:
-    #get data for track
-    name = track['track_name']
-    album_name = track['album_name']
-    album_cover_url = track['album_cover']
-    release_date = track['release_date']
-    artists = track['artists']
-
+for track in res["tracks"]:
+    # get data for track
+    name = track["track_name"]
+    album_name = track["album_name"]
+    album_cover_url = track["album_cover"]
+    release_date = track["release_date"]
+    artists = track["artists"]
 
     if f"{name}.mp3" in os.listdir(save_path):
-        overwrite = input(f"Song named {name} already exists. Do you want to overwrite it: y/n?")
+        overwrite = input(
+            f"Song named {name} already exists. Do you want to overwrite it: y/n?"
+        )
         if overwrite.lower() == "y":
-            remove_file = os.path.join(save_path,f"{name}.mp3")
+            remove_file = os.path.join(save_path, f"{name}.mp3")
             os.remove(remove_file)
-
-            print("Starting download for: ", name)
-
-            #search the song on ytmusicapi
-            track_url = search_song(name)
-
-            #download song audio
-            audiofile = download_audio(track_url,name,save_path)
-            
-            #download song cover image
-            img_path = pathlib.Path(save_path)
-            #print(img_path)
-            cover_img = download_image(album_cover_url,img_path,name)
-
-            #converting the file to mp3
-            convert_audio(audiofile, os.path.join(save_path,f"{name}.mp3"))
-
-            #add tags to audio file
-            final_path = os.path.join(save_path,f"{name}.mp3")
-            add_tags(final_path, name, album_name, release_date, artists, cover_img)
-
         else:
             print(f"Terminating download for song {name}.")
+            sys.exit()
+
+    print("Starting download for: ", name)
+
+    # search the song on ytmusicapi
+    track_url = search_song(name)
+
+    # download song audio
+    audiofile = download_audio(track_url, name, save_path)
+
+    # download song cover image
+    img_path = pathlib.Path(save_path)
+    # print(img_path)
+    cover_img = download_image(album_cover_url, img_path, name)
+
+    # converting the file to mp3
+    convert_audio(audiofile, os.path.join(save_path, f"{name}.mp3"))
+
+    # add tags to audio file
+    final_path = os.path.join(save_path, f"{name}.mp3")
+    add_tags(final_path, name, album_name, release_date, artists, cover_img)
 
 
-
-#links to test
-#https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02?si=8985b2d7459f4eff
-#https://open.spotify.com/track/3USxtqRwSYz57Ewm6wWRMp?si=99eb3680638d4189
-#https://open.spotify.com/album/5bfpRtBW7RNRdsm3tRyl3R?si=392fe62b8da144c7
-#https://open.spotify.com/playlist/3AqXePZ5Lw7tChvgFfAABE?si=80d104c9f2074089
+# links to test
+# https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02?si=8985b2d7459f4eff
+# https://open.spotify.com/track/3USxtqRwSYz57Ewm6wWRMp?si=99eb3680638d4189
+# https://open.spotify.com/album/5bfpRtBW7RNRdsm3tRyl3R?si=392fe62b8da144c7
+# https://open.spotify.com/playlist/3AqXePZ5Lw7tChvgFfAABE?si=80d104c9f2074089
--- src/spotify.py
+++ src/spotify.py
@@ -2,60 +2,63 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
 class Spot:
     def __init__(self, client_id, client_secret):
-        auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-        self.sp = spotipy.Spotify(auth_manager=auth_manager) 
+        auth_manager = SpotifyClientCredentials(
+            client_id=client_id, client_secret=client_secret
+        )
+        self.sp = spotipy.Spotify(auth_manager=auth_manager)
 
     def get_info(self, link):
-        #check what type of url
-        l = link.split('/')
+        # check what type of url
+        l = link.split("/")
 
         if l[3] == "track":
             results = self.sp.track(link)
             artists = ""
-            for a in results['artists']:
+            for a in results["artists"]:
                 artists = artists + f"{a['name']} "
 
-            #track info
-            tracklist = [{
-                'track_name':results['name'],
-                'release_date':results['album']['release_date'],
-                'artists':artists,
-                'album_name':results['album']['name'],
-                'album_cover':results['album']['images'][0]['url']
-            }]
+            # track info
+            tracklist = [
+                {
+                    "track_name": results["name"],
+                    "release_date": results["album"]["release_date"],
+                    "artists": artists,
+                    "album_name": results["album"]["name"],
+                    "album_cover": results["album"]["images"][0]["url"],
+                }
+            ]
 
-            track = {
-                'tracks': tracklist
-            }
+            track = {"tracks": tracklist}
             return track
 
         elif l[3] == "artist":
             results = self.sp.artist(link)
             artist_top_tracks = self.sp.artist_top_tracks(link)
-            #print(artist_top_tracks)
+            # print(artist_top_tracks)
             top_tracks = []
 
-            for t in artist_top_tracks['tracks']:
+            for t in artist_top_tracks["tracks"]:
                 artists = ""
-                for a in t['artists']:
+                for a in t["artists"]:
                     artists = artists + f"{a['name']} "
 
                 trackitem = {
-                    'track_name':t['name'],
-                    'release_date':t['album']['release_date'],
-                    'album_name':t['album']['name'],
-                    'album_cover':t['album']['images'][0]['url'],
-                    'artists':artists
+                    "track_name": t["name"],
+                    "release_date": t["album"]["release_date"],
+                    "album_name": t["album"]["name"],
+                    "album_cover": t["album"]["images"][0]["url"],
+                    "artists": artists,
                 }
                 top_tracks.append(trackitem)
 
             artist = {
-                'name':results['name'],
-                'followers':results['followers']['total'],
-                'genres':results['genres'],
-                'tracks':top_tracks
+                "name": results["name"],
+                "followers": results["followers"]["total"],
+                "genres": results["genres"],
+                "tracks": top_tracks,
             }
             return artist
 
@@ -63,32 +66,32 @@ class Spot:
             results = self.sp.album(link)
             tracks = []
             album_artists = []
-            for a in results['artists']:
-                album_artists.append(a['name'])
+            for a in results["artists"]:
+                album_artists.append(a["name"])
 
-            for t in results['tracks']['items']:
-                #get all artists
+            for t in results["tracks"]["items"]:
+                # get all artists
                 artists = ""
-                for a in t['artists']:
+                for a in t["artists"]:
                     artists = artists + f"{a['name']} "
 
                 trackitem = {
-                    'track_name':t['name'],
-                    'release_date':results['release_date'],
-                    'album_name':results['name'],
-                    'album_cover':results['images'][0]['url'],
-                    'artists':artists
+                    "track_name": t["name"],
+                    "release_date": results["release_date"],
+                    "album_name": results["name"],
+                    "album_cover": results["images"][0]["url"],
+                    "artists": artists,
                 }
                 tracks.append(trackitem)
 
             album = {
-                'name':results['name'],
-                'release_date':results['release_date'],
-                'total_tracks':results['total_tracks'],
-                'album_cover':results['images'][0]['url'],
-                'label':results['label'],
-                'album_artists':album_artists,
-                'tracks':tracks
+                "name": results["name"],
+                "release_date": results["release_date"],
+                "total_tracks": results["total_tracks"],
+                "album_cover": results["images"][0]["url"],
+                "label": results["label"],
+                "album_artists": album_artists,
+                "tracks": tracks,
             }
             return album
 
@@ -96,31 +99,29 @@ class Spot:
             results = self.sp.playlist(link)
             track_list = []
 
-            for t in results['tracks']['items']:
-                #get all artists
+            for t in results["tracks"]["items"]:
+                # get all artists
                 artists = ""
-                for a in t['track']['artists']:
+                for a in t["track"]["artists"]:
                     artists = artists + f"{a['name']} "
 
-                #details of each individual track
+                # details of each individual track
                 trackitem = {
-                    'track_name':t['track']['name'],
-                    'album_name':t['track']['album']['name'],
-                    'release_date':t['track']['album']['release_date'],
-                    'artists':artists,
-                    'album_cover':t['track']['album']['images'][0]['url']
+                    "track_name": t["track"]["name"],
+                    "album_name": t["track"]["album"]["name"],
+                    "release_date": t["track"]["album"]["release_date"],
+                    "artists": artists,
+                    "album_cover": t["track"]["album"]["images"][0]["url"],
                 }
                 track_list.append(trackitem)
 
-            #final dictionary of the playlist
+            # final dictionary of the playlist
             playlist = {
-                'id': results['id'],
-                'name': results['name'],
-                'description': results['description'],
-                'owner_name':results['owner']['display_name'],
-                'total_tracks':results['tracks']['total'],
-                'tracks':track_list
+                "id": results["id"],
+                "name": results["name"],
+                "description": results["description"],
+                "owner_name": results["owner"]["display_name"],
+                "total_tracks": results["tracks"]["total"],
+                "tracks": track_list,
             }
             return playlist
-        
- 
